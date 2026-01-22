aa84387b517bc934a63fe3b6bb2911b02174d55d
Nikola Hristov <Nikola@PlayForm.Cloud>
2026-01-22 19:11:31 +0200

    save
    Signed-off-by: Nikola Hristov <Nikola@PlayForm.Cloud>

---
+++ doc/trait.impl/core/marker/trait.Unpin.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["Maintain",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Unpin.html\" title=\"trait core::marker::Unpin\">Unpin</a> for <a class=\"enum\" href=\"Maintain/Build/enum.Error.html\" title=\"enum Maintain::Build::Error\">Error</a>",0,1,["Maintain::Build::Error"]],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Unpin.html\" title=\"trait core::marker::Unpin\">Unpin</a> for <a class=\"struct\" href=\"Maintain/Build/struct.Argument.html\" title=\"struct Maintain::Build::Argument\">Argument</a>",0,1,["Maintain::Build::Argument"]],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Unpin.html\" title=\"trait core::marker::Unpin\">Unpin</a> for <a class=\"struct\" href=\"Maintain/Build/struct.Guard.html\" title=\"struct Maintain::Build::Guard\">Guard</a>",0,1,["Maintain::Build::Guard"]],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Unpin.html\" title=\"trait core::marker::Unpin\">Unpin</a> for <a class=\"struct\" href=\"Maintain/Build/struct.Manifest.html\" title=\"struct Maintain::Build::Manifest\">Manifest</a>",0,1,["Maintain::Build::Manifest"]],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Unpin.html\" title=\"trait core::marker::Unpin\">Unpin</a> for <a class=\"struct\" href=\"Maintain/Build/struct.Meta.html\" title=\"struct Maintain::Build::Meta\">Meta</a>",0,1,["Maintain::Build::Meta"]]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[1493]}
---
+++ doc/trait.impl/core/marker/trait.UnsafeUnpin.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["Maintain",[["impl UnsafeUnpin for <a class=\"enum\" href=\"Maintain/Build/enum.Error.html\" title=\"enum Maintain::Build::Error\">Error</a>",0,1,["Maintain::Build::Error"]],["impl UnsafeUnpin for <a class=\"struct\" href=\"Maintain/Build/struct.Argument.html\" title=\"struct Maintain::Build::Argument\">Argument</a>",0,1,["Maintain::Build::Argument"]],["impl UnsafeUnpin for <a class=\"struct\" href=\"Maintain/Build/struct.Guard.html\" title=\"struct Maintain::Build::Guard\">Guard</a>",0,1,["Maintain::Build::Guard"]],["impl UnsafeUnpin for <a class=\"struct\" href=\"Maintain/Build/struct.Manifest.html\" title=\"struct Maintain::Build::Manifest\">Manifest</a>",0,1,["Maintain::Build::Manifest"]],["impl UnsafeUnpin for <a class=\"struct\" href=\"Maintain/Build/struct.Meta.html\" title=\"struct Maintain::Build::Meta\">Meta</a>",0,1,["Maintain::Build::Meta"]]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[868]}
